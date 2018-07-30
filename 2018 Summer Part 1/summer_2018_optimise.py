d5cc570354f5a68c0f0f620cc7d27b8ff96d4c4f
Kenton Lam <kenton_lam@outlook.com>
2018-07-30 22:55:29 +1000

    Refactored into classes and appropriate functions.

--- 2018 Summer Part 1/summer_2018_optimise.py
+++ 2018 Summer Part 1/summer_2018_optimise.py
@@ -3,114 +3,146 @@ import numpy
 from collections import OrderedDict
 import json
 import typing
+import types
 
-required_materials = {
-    'water': 0,
-    'food': 300,
-    'wood': 300,
-    'stone': 300,
-    'iron': 300
-}
+class Items(dict):
+    def __init__(self, water=0, food=0, wood=0, stone=0, iron=0):
+        mapping = {
+            'water': water,
+            'food': food,
+            'wood': wood,
+            'stone': stone,
+            'iron': iron
+        }
+        for k, v in mapping.items():
+            setattr(self, k, v)
+        super().__init__(mapping)
 
-have = {
-    'water': 440,
-    'food': 769,
-    'wood': 28, 
-    'stone': 110,
-    'iron': 409
-}
+class EventOptimiser:
 
-short = {
-    'water': 1100,
-    'food': 2800,
-    'wood': 300,
-    'stone': 2800,
-    'iron': 1700
-}
+    def __init__(self):
+        self._required = Items()
+        self._target = Items()
+        self._current = Items()
+        self._farming_nodes = OrderedDict()
 
-required_materials = {}
-for mat, num in short.items():
-    required_materials[mat] = max(num - have[mat], 0)
+    def _update_required(self):
+        self._required.clear()
+        for mat, num in self._target.items():
+            self._required[mat] = max(num - self._current[mat], 0)
 
-with open('2018 Summer Part 1/currency_drops_parsed.json') as f:
-    data = json.decoder.JSONDecoder().decode(f.read())
+    def set_target(self, items: Items):
+        self._target = items
+        self._update_required()
 
-def drops(location, water=0, food=0, wood=0, stone=0, iron=0):
-    location_drops = data[location + ' storm']
-    drops = {}
-    mapping = {
-        'water': water,
-        'food': food,
-        'wood': wood,
-        'stone': stone,
-        'iron': iron
-    }
-    for mat, bonus in mapping.items():
-        if mat in location_drops:
-            drops[mat] = location_drops[mat][bonus]
-        elif bonus != 0:
-            print('='*10)
-            print('Servant/CE warning: You have a', mat, 'bonus but', location, 'does not drop', mat+'!')
-            print('='*10)
-    return drops
+    def set_current(self, items: Items):
+        self._current = items
+        self._update_required()
 
-summertime_mistresses = s = 4
-wood_ces = w = 1
+    def set_farming_nodes(self, nodes):
+        self._farming_nodes = nodes
 
-materials_per_run = OrderedDict((
-    # assuming +6 bonus on each single material, and +2 of each for mountains.
-    ('beach', drops('beach', water=6+s, food=0+s)),
-    ('forest', drops('forest', food=6+s, water=0+s)), # Primeval Forest
-    ('jungle', drops('jungle', wood=6+w, food=0+s)), # Jungle
-    ('field', drops('field', stone=6, food=0+s)), # Grassfields
-    ('cave', drops('cavern', iron=6, water=0+s)),
-    ('mountains', drops('mountains', wood=2+w, stone=2, iron=2)),
-))
+    def _runs_required(self, node_ratios):
+        ratios = self._to_dict(node_ratios)
+        materials_per_iteration = self.total_materials(ratios)
 
-'''Returns the AP required to obtain the required materials, assuming quests
-are done in the ratio specified in `params`.
+        required_iterations = 0
+        for mat, drops_per_iter in materials_per_iteration.items():
+            multiplier = self._required[mat] / drops_per_iter
+            if multiplier > required_iterations:
+                required_iterations = multiplier
 
-`params` is a tuple representing the ratio in which the nodes will be farmed.
-It must have the same dimension as the specified farming locations.
+        return [required_iterations*x for x in node_ratios]
 
-Returns:
-    float -- Decimal AP required.
-'''
-def ap_required(params: typing.Tuple[float, ...], log=False) -> float:
-    ratios = {}
-    for i, loc in enumerate(materials_per_run):
-        ratios[loc] = params[i]
-    materials_per_iteration = {}
-    for loc, drops in materials_per_run.items():
-        for mat, mat_drops in drops.items():
-            if mat not in materials_per_iteration:
-                materials_per_iteration[mat] = 0
-            materials_per_iteration[mat] += ratios[loc] * mat_drops
+    def _ap_required(self, node_ratios):
+        runs = self._runs_required(node_ratios)
+        return 40*sum(runs)
 
-    required_iterations = 0
-    limiting = None
-    for mat, drops_per_iter in materials_per_iteration.items():
-        multiplier = required_materials[mat] / drops_per_iter
-        if multiplier > required_iterations:
-            required_iterations = multiplier
-            limiting = mat
-    if log:
-        print(materials_per_run)
-        print('=== Results ===')
-        # print('final materials')
-        # for mat, drops in materials_per_iteration.items():
-        #     print(' ', mat, drops*required_iterations)
-        print('Required Runs:')
-        for loc, r in ratios.items():
-            print(('  '+loc.title()).ljust(12), '{:>8.2f}'.format(r*required_iterations))
-    return required_iterations*40*sum(params)
+    def _to_dict(self, array):
+        d = {}
+        for i, key in enumerate(self._farming_nodes):
+            d[key] = array[i]
+        return d
+
+    def _do_optimise(self):
+        n = len(self._farming_nodes)
+        bounds = optimize.Bounds([0]*n, [numpy.inf]*n, True)
+        return optimize.minimize(self._ap_required, [10]*n, 
+            method='SLSQP', bounds=bounds)
+
+    def optimise_runs(self):
+        result = self._do_optimise()
+        return self._to_dict(self._runs_required(result.x))
+
+    def total_materials(self, nodes_farmed):
+        mats = {}
+        for loc, times in nodes_farmed.items():
+            for mat, mat_drops in self._farming_nodes[loc].items():
+                if mat not in mats:
+                    mats[mat] = 0
+                mats[mat] += times * mat_drops
+        return mats
+
+class DropsData: 
+    def __init__(self, data):
+        self._data = data
+
+    def drops(self, location, bonuses: Items=None, **kwargs):
+        location_drops = self._data[location]
+        drops = {}
+        if bonuses is None:
+            bonuses = Items(**kwargs)
+
+        for item, bonus in bonuses.items():
+            if item in location_drops:
+                drops[item] = location_drops[item][bonus]
+            elif bonus != 0:
+                raise Warning(
+                    'Servant/CE warning: You have a '+item+' bonus but '+location+' does not drop '+item+'!')
+        return drops
 
 def _main():
-    n = len(materials_per_run)
-    bounds = optimize.Bounds([0]*n, [numpy.inf]*n, True)
-    result = optimize.minimize(ap_required, [10]*n, method='SLSQP', bounds=bounds)
-    #print(result)
-    print('Required AP:  '+ '{:7.2f}'.format(ap_required(result.x, True)))
+    with open('2018 Summer Part 1/currency_drops_parsed.json') as f:
+        raw_data = json.decoder.JSONDecoder().decode(f.read())
+    data = DropsData(raw_data)
+
+    summertime_mistresses = s = 4
+    wood_ces = w = 1
+
+    nodes = OrderedDict((
+        # assuming +6 bonus on each single material, and +2 of each for mountains.
+        ('beach', data.drops('beach storm', water=6+s, food=0+s)),
+        ('forest', data.drops('forest storm', food=6+s, water=0+s)), # Primeval Forest
+        ('jungle', data.drops('jungle storm', wood=6+w, food=0+s)), # Jungle
+        ('field', data.drops('field storm', stone=6, food=0+s)), # Grassfields
+        ('cave', data.drops('cavern storm', iron=6, water=0+s)),
+        ('mountains', data.drops('mountains storm', wood=2+w, stone=2, iron=2)),
+    ))
+
+    event_opt = EventOptimiser()
+    event_opt.set_current(Items(
+        water=440,
+        food=769,
+        wood=28,
+        stone=110,
+        iron=409
+    ))
+    event_opt.set_target(Items(
+        water=1100,
+        food=2800,
+        wood=300,
+        stone=2800,
+        iron=1700
+    ))
+    event_opt.set_farming_nodes(nodes)
+
+    runs = event_opt.optimise_runs()
+    print(runs)
+    print('Required AP:', 40*sum(runs.values()))
+    print()
+    print('Materials')
+    print(event_opt._required)
+    print(event_opt.total_materials(runs))
 
 if __name__ == '__main__':
     _main()
